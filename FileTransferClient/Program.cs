7aa1a08d4032c4d55dacfde70c56772e371d1169
Burrito119 <kevinliger13@gmail.com>
2015-09-03 00:34:38 -0400

    getting ready to revert

--- FileTransferClient/Program.cs
+++ FileTransferClient/Program.cs
@@ -14,16 +14,12 @@ namespace FileTransferClient
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Welcome to Burrito's file transfer system!");
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
 
-            // TODO: Implement Functionality Here
-
-            Console.WriteLine("Enter Desired IP: ");
-            String ip = Console.ReadLine();
-            if (ip.Contains("local"))
-                new Client().connect(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 28889));
-            else
-                new	Client().connect(new System.Net.IPEndPoint(System.Net.IPAddress.Parse(ip), 28889));
+			
+			new	Client().connect(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 28889));
 		}
 	}
 }
